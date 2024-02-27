6c5db3a930b3331ca74fb2257072908dba300a27
Samuel Young <samyoung12788@gmail.com>
2024-02-26 18:57:51 -0600

    unzip.h comments

--- src/unzip.h
+++ src/unzip.h
@@ -1,6 +1,13 @@
+/*
+ * This is the default path max for zip files in Windows. I'd assume epub
+ * creators would not exceed this limit.
+ */
 #define ZIP_PATH_MAX 260
 
+/* Basically just rm -r */
 void uz_rm_tree(char* path);
+
+/* Basically just mkdir -p */
 int  uz_make_path(char* path);
 
 /* Unzips contents of epub to outputdir */
