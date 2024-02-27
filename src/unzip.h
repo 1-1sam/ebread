5c0ac8984a1b7dba66fe4708676767b70923ba22
Samuel Young <samyoung12788@gmail.com>
2024-02-26 18:58:24 -0600

    Changed header function spacing

--- src/unzip.h
+++ src/unzip.h
@@ -8,7 +8,7 @@
 void uz_rm_tree(char* path);
 
 /* Basically just mkdir -p */
-int  uz_make_path(char* path);
+int uz_make_path(char* path);
 
 /* Unzips contents of epub to outputdir */
 /* NOTE: output_dir must end with a slash character */
